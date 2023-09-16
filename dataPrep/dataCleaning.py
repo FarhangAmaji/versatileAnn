28aaa9eac23e0ea269956e506f33bed4ab8fccdb
FarhangAmaji <popamaji@gmail.com>
2023-09-16 17:06:18 +0330

    refactor: noNanOrNoneData

--- dataPrep/dataCleaning.py
+++ dataPrep/dataCleaning.py
@@ -3,20 +3,32 @@ import torch
 import pandas as pd
 import numpy as np
 #%%
+def noNanOrNoneDf(data):
+    assert isinstance(data, (pd.DataFrame, pd.Series))
+    if data.isna().any().any():
+        raise ValueError("The DataFrame contains NaN values.")
+
+def noNanOrNoneNpArray(data):
+    assert isinstance(data, np.ndarray)
+    if np.isnan(data).any():
+        raise ValueError("The NumPy array contains NaN values.")
+
+def noNanOrNoneNpDict(data):
+    assert isinstance(data, NpDict)
+    if data.df.isna().any().any():
+        raise ValueError("The NpDict contains NaN values.")
+
+def noNanOrNoneTensor(data):
+    assert isinstance(data, torch.Tensor)
+    if torch.isnan(data).any().item():
+        raise ValueError("The PyTorch tensor contains NaN values.")
+
 def noNanOrNoneData(data):
     if isinstance(data, (pd.DataFrame, pd.Series)):
-        if data.isna().any().any():
-            raise ValueError("The DataFrame contains NaN values.")
-    
+        noNanOrNoneDf(data)
     elif isinstance(data, np.ndarray):
-        if np.isnan(data).any():
-            raise ValueError("The NumPy array contains NaN values.")
-
+        noNanOrNoneNpArray(data)
     elif isinstance(data, NpDict):
-        data=data[:]
-        if np.isnan(data).any():
-            raise ValueError("The NpDict array contains NaN values.")
-
+        noNanOrNoneNpDict(data)
     elif isinstance(data, torch.Tensor):
-        if torch.isnan(data).any().item():
-            raise ValueError("The PyTorch tensor contains NaN values.")
+        noNanOrNoneTensor(data)
