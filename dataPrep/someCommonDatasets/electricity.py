54b9f6d79f50aa22587e2f68cfc63c372b785547
FarhangAmaji <popamaji@gmail.com>
2023-09-17 19:48:37 +0330

    feat: someCommonDatasets: electricity

---
+++ dataPrep/someCommonDatasets/electricity.py
@@ -0,0 +1,56 @@
+#%% imports
+"""
+this is data preparation steps of electricity of 369 consumers
+data also can be found at https://archive.ics.uci.edu/dataset/321/electricityloaddiagrams20112014
+this data has ['date', 'consumerId', 'hourOfDay', 'dayOfWeek', 'powerUsage','daysFromStart', 
+               'hoursFromStart', 'dayOfMonth', 'month'] columns
+there are many date cols and consumerId and powerUsage cols
+this dataset has different consumer data which are treated as separate data sequences(NSeries)
+"""
+from dataPrep.utils import getDatasetFiles, calculateNSeriesMinDifference, excludeValuesFromEnd_NSeries, splitTrainValTestNSeries
+from dataPrep.normalizers import NormalizerStack, SingleColsStdNormalizer, SingleColsLblEncoder
+from dataPrep.dataset import VAnnTsDataset
+from dataPrep.dataloader import VAnnTsDataloader
+from utils.globalVars import tsStartPointColName
+#%%
+embedderInputSize=369# 369 different consumerIds
+allReals=['hourOfDay', 'dayOfWeek', 'powerUsage','daysFromStart', 'hoursFromStart', 'daysFromStartOfDf', 'month']
+covariatesNum=len(allReals)
+#%%
+timeIdx='hoursFromStart'
+mainGroups=['consumerId']
+target=['powerUsage']
+backcastLen=192
+forecastLen=1 #ccc this is for the shift
+datasetKwargs={'mainGroups':mainGroups, 'consumerId':['consumerId'],'target':target}
+def getElectricityProcessed(backcastLen=192, forecastLen=1):
+    df =getDatasetFiles('electricity.csv')#kkk that this file exists when pulled
+    calculateNSeriesMinDifference(df, mainGroups, valueCol=timeIdx, resultCol='sequenceIdx')
+    excludeValuesFromEnd_NSeries(df, mainGroups, backcastLen+forecastLen, valueCol='sequenceIdx', resultCol=tsStartPointColName)
+    normalizer=NormalizerStack(SingleColsStdNormalizer(['powerUsage','daysFromStart', 'hoursFromStart', 'dayOfMonth']),
+                    SingleColsLblEncoder(mainGroups))
+    trainDf, valDf, testDf=splitTrainValTestNSeries(df, mainGroups, trainRatio=.7, valRatio=.2, seqLen=backcastLen+forecastLen, shuffle=True)
+    return trainDf, valDf, testDf, normalizer
+#%% 
+class ElectricityDeepArDataset(VAnnTsDataset):
+    def __getitem__(self, idx):
+        inputs={}
+        inputs['consumerId']=self.getBackForeCastData(self.data, idx, mode=self.modes.singlePoint, colsOrIndexes=self.consumerId)
+        inputs['allReals']=self.getBackForeCastData(self.data, idx, mode=self.modes.backcast, colsOrIndexes=self.allReals)
+        inputs['target']=self.getBackForeCastData(self.data, idx, mode=self.modes.backcast, colsOrIndexes=self.target, shiftForward=0)
+
+        outputs=self.getBackForeCastData(self.data, idx, mode=self.modes.backcast, colsOrIndexes=self.target, shiftForward=1)
+        return inputs, outputs
+#%% dataloader
+def getElectricityDataloaders(backcastLen=192, forecastLen=1, batchSize=64):
+    trainDf, valDf, testDf, normalizer=getElectricityProcessed(backcastLen=backcastLen, forecastLen=forecastLen)
+
+    electricityDeepArTrainDataset=ElectricityDeepArDataset(trainDf, backcastLen=backcastLen, forecastLen=forecastLen, indexes=None, **datasetKwargs)
+    electricityDeepArValDataset=ElectricityDeepArDataset(valDf, backcastLen=backcastLen, forecastLen=forecastLen, indexes=None, **datasetKwargs)
+    electricityDeepArTestDataset=ElectricityDeepArDataset(testDf, backcastLen=backcastLen, forecastLen=forecastLen, indexes=None, **datasetKwargs)
+    del trainDf, valDf, testDf
+
+    electricityDeepArTrainDataloader=VAnnTsDataloader(electricityDeepArTrainDataset, batch_size=batchSize)
+    electricityDeepArValDataloader=VAnnTsDataloader(electricityDeepArValDataset, batch_size=batchSize)
+    electricityDeepArTestDataloader=VAnnTsDataloader(electricityDeepArTestDataset, batch_size=batchSize)
+    return electricityDeepArTrainDataloader, electricityDeepArValDataloader, electricityDeepArTestDataloader, normalizer
