0888526655b8047b0dbfe224a49b74f71a18686e
FarhangAmaji <popamaji@gmail.com>
2024-01-14 23:27:37 +0330

    feat: _NewWrapper_preInitNPostInit_nModelReset

---
+++ versatileAnn/newModule/preInitNPostInit_nModelReset.py
@@ -0,0 +1,13 @@
+class _NewWrapper_preInitNPostInit_nModelReset(_NewWrapper_preInitNPostInit_nModelReset_inner):
+    classesCalledBy_init_subclass_ = []
+    _parentClasses_tillNewWrapper_inits = None
+
+    def __init_subclass__(cls, **kwargs):
+        _NewWrapper_preInitNPostInit_nModelReset.classesCalledBy_init_subclass_.append(cls)
+    def __new__(cls, **kwargs):
+        # cccDevStruct
+        #  - __new__ acts as `preInit` step, also enables to have `postInit`.
+        print(f'NewWrapper __new__ method initiated for "{cls.__name__}" class')
+
+        # we know the first item in .classesCalledBy_init_subclass_ is the NewWrapper class object
+        _NewWrapper_Obj = cls.classesCalledBy_init_subclass_[0]
