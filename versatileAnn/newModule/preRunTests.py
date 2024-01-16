ef7c37eac48a5400588178e8e268c4d3c7176097
FarhangAmaji <popamaji@gmail.com>
2024-01-16 22:03:19 +0330

    feat: findBestLearningRate

--- versatileAnn/newModule/preRunTests.py
+++ versatileAnn/newModule/preRunTests.py
@@ -1,5 +1,15 @@
-from abc import ABC
-from typing import List, Union
+import os
+from typing import List
+
+import pytorch_lightning as pl
+from pytorch_lightning.profilers import PyTorchProfiler
+from torch import nn
+
+from utils.typeCheck import argValidator
+from utils.vAnnGeneralUtils import getMethodRelatedKwargs, morePreciseFloat
+from utils.warnings import Warn
+from versatileAnn.newModule.callbacks import StoreEpochData
+
 
 class _NewWrapper_preRunTests:
     def __init__(self, **kwargs):
@@ -8,13 +18,14 @@ class _NewWrapper_preRunTests:
     @argValidator
     def preRunTests(self, trainDataloader, *, losses: List[nn.modules.loss._Loss], maxEpochs=5,
                     savePath, tensorboardPath='', valDataloader=None, lrRange=(1e-6, 5),
-                    lrNumSteps=20, lrsToFindBest=None, externalKwargs=None, fastDevRunKwargs=None,
-                    overfitBatchesKwargs=None, profilerKwargs=None,
-                    **kwargs):
+                    lrNumSteps=20, lrsToFindBest=None, fastDevRunKwargs=None,
+                    overfitBatchesKwargs=None, profilerKwargs=None, findBestLearningRateKwargs=None,
+                    findBestBatchSizesKwargs=None, **kwargs):
         # kkk correct this args
         fastDevRunKwargs = fastDevRunKwargs or {}
         overfitBatchesKwargs = overfitBatchesKwargs or {}
         profilerKwargs = profilerKwargs or {}
+        findBestLearningRateKwargs = findBestLearningRateKwargs or {}
         kwargsRelatedToTrainer = getMethodRelatedKwargs(pl.Trainer, kwargs, delAfter=True)
 
         self.runFastDevRun(fastDevRunKwargs, kwargsRelatedToTrainer,
@@ -24,6 +35,11 @@ class _NewWrapper_preRunTests:
         trainer = self.runProfiler(kwargsRelatedToTrainer, profilerKwargs,
                                    trainDataloader, valDataloader)
 
+        self.findBestLearningRate(trainDataloader, valDataloader,
+                                  lrRange=lrRange, numSteps=lrNumSteps,
+                                  lrsToFindBest=lrsToFindBest,
+                                  findBestLearningRateKwargs=findBestLearningRateKwargs,
+                                  kwargsRelatedToTrainer=kwargsRelatedToTrainer)  # kkk2 add kwargs also for this
     def runFastDevRun(self, fastDevRunKwargs, kwargsRelatedToTrainer, trainDataloader,
                       valDataloader):
         # cccDevAlgo
@@ -88,4 +104,49 @@ class _NewWrapper_preRunTests:
 
         # trainer is returned to be able to print(in _printTensorboardPath) where users can use tensorboard
         return trainer
+
+    def findBestLearningRate(self, trainDataloader, valDataloader, lrRange=(1e-6, 5), numSteps=20,
+                             lrsToFindBest=None, findBestLearningRateKwargs=None,
+                             kwargsRelatedToTrainer=None):
+        # cccUsage
+        #  takes lr ranges either with (lrRange, numSteps) or with (lrsToFindBest)
+        if lrsToFindBest:
+            lrs = lrsToFindBest
+        else:
+            lrUpdateStep = (lrRange[1] / lrRange[0]) ** (1 / numSteps)
+            # confines lrs to a precision of 6 digits, also with using set, only unique values are kept
+            lrs = {morePreciseFloat(lrRange[0] * (lrUpdateStep ** step)) for step in
+                   range(numSteps)}
+        pastLr = self.lr
+        lossRatioDecrease = {}
+
+        kwargsApplied = {'max_epochs': 4, 'enable_checkpointing': False, 'logger': False, }
+        self._getKwargsApplied_forRelatedRun(kwargsRelatedToTrainer, findBestLearningRateKwargs,
+                                             kwargsApplied)
+
+        mainValLossName = self._getLossName('val', self.losses[0])
+        for thisLr in lrs:
+            self = self.resetModel()
+            self.resetOptimizer()  # to be sure that past accumulated params like momentum have got reset
+            self.changeLearningRate(thisLr)
+
+            callbacks_ = [StoreEpochData()]
+            kwargsApplied['callbacks'] = callbacks_
+            trainer = pl.Trainer(**kwargsApplied)
+            # goodToHave1
+            #  contextManger to disable progressBar temporarily
+            trainer.fit(self, trainDataloader, valDataloader)
+            self._collectMetricsOfRun(callbacks_, lossRatioDecrease, mainValLossName,
+                                      thisLr)
+
+        bestLearningRate = min(lossRatioDecrease, key=lambda k: lossRatioDecrease[k]['score'])
+        worstLearningRate = max(lossRatioDecrease, key=lambda k: lossRatioDecrease[k]['score'])
+        Warn.info(f'bestLearningRate is {bestLearningRate} and the worst being {worstLearningRate}')
+
+        if not self.keepLr_notReplaceWithBestLr:
+            self.lr = bestLearningRate
+        else:
+            self.lr = pastLr
+        # goodToHave2
+        #  add ploting in tensorboard with a message that plot is in tensorboard
 
