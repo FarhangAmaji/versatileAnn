c1c624c9fc39a3f317e60660b5277e9e823830d4
FarhangAmaji <popamaji@gmail.com>
2024-01-17 21:31:13 +0330

    test: optimizerTests

---
+++ tests/NewWrapperTests/optimizerTests.py
@@ -0,0 +1,85 @@
+import unittest
+
+import torch
+from torch import nn
+
+from tests.baseTest import BaseTestClass
+from versatileAnn.newModule.newWrapper import NewWrapper
+
+
+class newWrapperTests_optimizer(BaseTestClass):
+    def setUp(self):
+        class ChildClass(NewWrapper):
+            def __init__(self, **kwargs):
+                self.l1 = nn.Linear(1, 1)
+
+            def forward(self, inputs, targets):
+                pass
+
+        return ChildClass
+
+    def setupSgdOpt(self):
+        ChildClass = self.setUp()
+        model = ChildClass()
+        model.optimizer = torch.optim.SGD(model.parameters(), lr=0.0001, momentum=0.9,
+                                          dampening=0, weight_decay=0, nesterov=False)
+        return model
+
+    def testDefaultOptimizerSetup(self):
+        def testFunc():
+            ChildClass = self.setUp()
+            model = ChildClass(testPrints=True)
+            self.assertTrue(isinstance(model.optimizer, torch.optim.Adam))
+
+        expectedPrint = """NewWrapper __new__ method initiated for "ChildClass" class
+no optimizer was set, a default Adam optimizer with lr=0.0003 was set
+_NewWrapper_postInit func
+ChildClass
+"""
+        self.assertPrint(testFunc, expectedPrint)
+
+    def test_OptimizerInitArgs1(self):
+        ChildClass = self.setUp()
+        model = ChildClass()
+        model.optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, betas=(0.9, 0.999),
+                                           eps=1e-08)
+
+        self.assertEqual(model._optimizerInitArgs['type'], torch.optim.Adam)
+        initArgs_args = {'lr': 0.0001, 'betas': (0.9, 0.999), 'eps': 1e-08, 'weight_decay': 0,
+                         'amsgrad': False, 'maximize': False, 'foreach': None, 'capturable': False,
+                         'differentiable': False, 'fused': None}
+        self.assertEqual(model._optimizerInitArgs['args'], initArgs_args)
+
+    def test_OptimizerInitArgs2(self):
+        model = self.setupSgdOpt()
+
+        self.assertEqual(model._optimizerInitArgs['type'], torch.optim.SGD)
+        initArgs_args = {'lr': 0.0001, 'momentum': 0.9, 'dampening': 0, 'weight_decay': 0,
+                         'nesterov': False, 'maximize': False, 'foreach': None,
+                         'differentiable': False}
+        self.assertEqual(model._optimizerInitArgs['args'], initArgs_args)
+
+    def test_changingSelfLr_changesOptimizerLr(self):
+        model = self.setupSgdOpt()
+
+        model.lr = .007
+        self.assertEqual(model.optimizer.param_groups[0]['lr'], model.lr)
+
+    def test_OptimizerReset_keepLr(self):
+        model = self.setupSgdOpt()
+
+        model.lr = .007
+        model.resetOptimizer()
+        self.assertEqual(model.optimizer.param_groups[0]['lr'], model.lr)
+
+    def test_OptimizerReset_noKeepLr(self):
+        model = self.setupSgdOpt()
+
+        model.lr = .007
+        model.resetOptimizer(keepLr=False)
+        self.assertEqual(model.optimizer.param_groups[0]['lr'], 0.0001)
+
+
+# ---- run test
+if __name__ == '__main__':
+    unittest.main()
--- tests/NewWrapperTests/preInitNPostInit_nModelResetTests.py
+++ tests/NewWrapperTests/preInitNPostInit_nModelResetTests.py
@@ -76,7 +76,9 @@ GrandChild;Before calling Child __init__ GrandChild
 emptyMethod_usedForDisabling__init__s
 emptyMethod_usedForDisabling__init__s
 GrandChild __init__;After calling Child __init__ GrandChild
-_NewWrapper_postInit func GrandChild
+no optimizer was set, a default Adam optimizer with lr=0.0003 was set
+_NewWrapper_postInit func
+GrandChild
 """
         self.assertPrint(testFunc, expectedPrint)
 
