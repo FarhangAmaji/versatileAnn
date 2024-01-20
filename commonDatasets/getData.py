c34b4138adbf70bc0dca4df75c7b0517e538992e
FarhangAmaji <popamaji@gmail.com>
2024-01-21 03:21:51 +0330

    fix: downloading datasets uploaded with gitlens

--- commonDatasets/getData.py
+++ commonDatasets/getData.py
@@ -1,10 +1,11 @@
 import asyncio
 import os
+import subprocess
 
-import aiohttp
 import pandas as pd
 
 from utils.vAnnGeneralUtils import downloadFileAsync
+from utils.warnings import Warn
 
 datasetsRelativePath = r'.\data\datasets'
 knownDatasets_dateTimeCols = {
@@ -21,7 +22,7 @@ knownDatasets = {"EPF_FR_BE.csv": "demand/EPF_FR_BE.csv",
                  }
 
 
-def getDatasetFiles(fileName: str, dateTimeCols=None, sortCols=None):
+def getDatasetFiles(fileName: str, dateTimeCols=None, sortCols=None, try_=0):
     dateTimeCols = dateTimeCols or []
     sortCols = sortCols or []
 
@@ -32,6 +33,17 @@ def getDatasetFiles(fileName: str, dateTimeCols=None, sortCols=None):
             _downloadMissingKnownDataset(fileName)
         else:
             raise Exception(f"File {fileName} does not exist in the data/datasets folder")
+
+    if isFileIn_gitlfsFormat(filePath):
+        # cccDevStruct
+        #  the files bigger than 100Mb are uploaded in github repos with gitlens
+        #  but this url pattern `https://raw.githubusercontent.com/FarhangAmaji/Datasets/main/...`
+        #  don't have csv data, instead they have sth like `version https://git-lfs.github.com/...`
+        #  so it's detected is this pattern exists or not and if yes uses _downloadFileWithCurl
+        #  which downloads from another url with curl
+        # delete file in filePath
+        os.remove(filePath)
+        _downloadFileWithCurl(fileName, filePath)
 
     df = pd.read_csv(filePath)
     _convertDatetimeNSortCols(dateTimeCols, df, fileName, sortCols)
@@ -47,6 +59,8 @@ def _getFilePathInDataStoreLocation(fileName):
 
 
 def _downloadMissingKnownDataset(fileName):
+    # cccDevStruct
+    #  this how we call an async function from a sync function
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(_downloadMissingKnownDataset_Async(fileName))
@@ -84,3 +98,52 @@ def _convertDf_DatetimeNSortCols(df, dateTimeCols, sortCols):
     df = df.sort_values(by=sortCols).reset_index(drop=True)
 
 
+# --- funcs to download files which are uploaded with gitlens
+def isFileIn_gitlfsFormat(filePath):
+    with open(filePath, 'r') as file:
+        first_line = file.readline().strip()
+        return first_line.startswith('version https://git-lfs.github.com/')
+
+
+async def _downloadFileWithCurl_async(url, filePath, event=None):
+    # goodToHave3
+    #  add printing online (changing with changes of cmd output)
+    Warn.info(f"starting to download and save: {filePath} from {url}")
+    Warn.info(f"this is probably a big file so be patient")
+
+    workingDirectory = os.path.dirname(filePath)
+    command = ['curl', '-LJO', url]
+    process = await asyncio.create_subprocess_exec(*command, stdout=subprocess.PIPE,
+                                                   stderr=subprocess.PIPE, cwd=workingDirectory)
+
+    stdout, stderr = await process.communicate()
+
+    if process.returncode == 0:
+        Warn.info(f"Downloaded and saved: {filePath}")
+
+    else:
+        errMsg = f"Failed to download {url}. Error: {stderr.decode('utf-8')}"
+        Warn.error(errMsg)
+        raise RuntimeError(errMsg)
+
+
+def _downloadFileWithCurl(fileName, filePath):
+    url = f'https://github.com/FarhangAmaji/Datasets/raw/main/demand/{fileName}'
+    # goodToHave2 # bugPotentialCheck1
+    #  this loop gets closed and gives error below at end of program
+    #  it's not a realy important error but keeps the loop open till the program is running
+    #  error:
+    #  """
+    #  File "C:\Program Files\Python310\lib\asyncio\proactor_events.py", line 116, in __del__
+    #     self.close()
+    #   File "C:\Program Files\Python310\lib\asyncio\proactor_events.py", line 108, in close
+    #     self._loop.call_soon(self._call_connection_lost, None)
+    #   File "C:\Program Files\Python310\lib\asyncio\base_events.py", line 745, in call_soon
+    #     self._check_closed()
+    #   File "C:\Program Files\Python310\lib\asyncio\base_events.py", line 510, in _check_closed
+    #     raise RuntimeError('Event loop is closed')
+    #  """
+    # Explicitly create and manage the event loop
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(_downloadFileWithCurl_async(url, filePath))
