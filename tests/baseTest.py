8207bae456fa8d46e36d2ea4da91e31f9b060122
FarhangAmaji <popamaji@gmail.com>
2024-01-21 19:23:48 +0330

    refactor: hard coding varPasser localArgs in BaseTestClass equal funcs

--- tests/baseTest.py
+++ tests/baseTest.py
@@ -24,17 +24,22 @@ class BaseTestClass(unittest.TestCase):
             sys.stdout = sys.__stdout__
 
     def equalDfs(self, df1, df2, checkIndex=True, floatApprox=False, floatPrecision=0.0001):
-        kwargs = varPasser()
-        self.assertTrue(equalDfs(**kwargs))
+        kwargs_ = varPasser(
+            localArgNames=['df1', 'df2', 'checkIndex', 'floatApprox', 'floatPrecision'])
+        self.assertTrue(equalDfs(**kwargs_))
 
     def equalArrays(self, array1, array2, checkType=True, floatApprox=False, floatPrecision=1e-4):
-        kwargs = varPasser()
-        self.assertTrue(equalArrays(**kwargs))
+        kwargs_ = varPasser(
+            localArgNames=['array1', 'array2', 'checkType', 'floatApprox', 'floatPrecision'])
+        self.assertTrue(equalArrays(**kwargs_))
 
     def equalTensors(self, tensor1, tensor2, checkType=True, floatApprox=False, floatPrecision=1e-4, checkDevice=True):
-        kwargs = varPasser()
-        self.assertTrue(equalTensors(**kwargs))
+        kwargs_ = varPasser(
+            localArgNames=['tensor1', 'tensor2', 'checkType', 'floatApprox', 'floatPrecision',
+                           'checkDevice'])
+        self.assertTrue(equalTensors(**kwargs_))
         
     def equalNpDicts(self, npd1, npd2, checkIndex=True, floatApprox=False, floatPrecision=0.0001):
-        kwargs = varPasser()
-        self.assertTrue(equalNpDicts(**kwargs))
+        kwargs_ = varPasser(
+            localArgNames=['npd1', 'npd2', 'checkIndex', 'floatApprox', 'floatPrecision'])
+        self.assertTrue(equalNpDicts(**kwargs_))
