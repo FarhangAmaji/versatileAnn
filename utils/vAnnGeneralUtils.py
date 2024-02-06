4c3a8b8777d54f36f555659cdf79de71354f8662
FarhangAmaji <popamaji@gmail.com>
2024-02-06 16:12:37 +0330

    feat: stringValuedDictsEqual

--- utils/vAnnGeneralUtils.py
+++ utils/vAnnGeneralUtils.py
@@ -477,6 +477,29 @@ def isNestedDict(dict_):
     return False
 
 
+@argValidator
+def stringValuedDictsEqual(dict1: dict, dict2: dict):
+    """
+    Check if two dictionaries with string values are equal.
+    """
+    for key, value in dict1.items():
+        if not isinstance(value, str):
+            raise ValueError(f"Value for key '{key}' is not a string.")
+
+    for key, value in dict2.items():
+        if not isinstance(value, str):
+            raise ValueError(f"Value for key '{key}' is not a string.")
+
+    if len(dict1) != len(dict2):
+        return False
+
+    for key, value in dict1.items():
+        if key not in dict2 or dict2[key] != value:
+            return False
+
+    return True
+
+
 # ---- methods and funcs
 def getMethodArgs(method):
     return list(inspect.signature(method).parameters.keys())
