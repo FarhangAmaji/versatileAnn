624dee2120b7dad892725b9d83a19c1c5a274a44
FarhangAmaji <popamaji@gmail.com>
2024-02-26 20:22:09 +0330

    docs: explain why runAllTests should be called from root folder

--- tests/runAllTests.py
+++ tests/runAllTests.py
@@ -1,7 +1,16 @@
 import os
 import sys
+
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
+# cccUsage
+#  note in few lines later I would explain why it's important to call this file from the root;
+#  but it's important to know that this file is called from the root
+# kkk for windows/linux/mac, add how to run this file in order to be called from the root folder
+# cccDevStruct
+#  note tests/brazingTorchTests/fitTests.py uses a func which uses pytorchLogger from
+#  pytorch lightning, and the path of that logger is dependent on where this code is called so
+#  by default this project is designed to be called from root folder
 import shutil
 import unittest
 
